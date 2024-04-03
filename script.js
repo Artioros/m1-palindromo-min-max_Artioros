44793c5d6b9b3b2c519e2de301b6d1ce68c1a9ea
Arthur Moreira <arthurmoreira49@gmail.com>
2024-04-03 15:24:01 -0300

    correção

--- script.js
+++ script.js
@@ -1,42 +1,41 @@
+
 function isPalindrome(str){
-    function isPalindrome(str){
-        let verdadeiro = false
+    let verdadeiro = false
     
-        function invertePalavras (str) {
-            str = str.toLowerCase()
-            str = str.replace(/\s/g,'')
-            let arr = str.split('')
-            let palavra = ''
+    function invertePalavras (str) {
+        str = str.toLowerCase()
+        str = str.replace(/\s/g,'')
+        let arr = str.split('')
+        let palavra = ''
         
-            for (let i = arr.length - 1; i >= 0 ; i--) {
-                palavra += arr[i]
-            }
+        for (let i = arr.length - 1; i >= 0 ; i--) {
+            palavra += arr[i]
+        }
         
-            return palavra
-        }
-    
-        function palavras (str) {
-            str = str.toLowerCase()
-            str = str.replace(/\s/g,'')
-            let arr = str.split('')
-            let palavra = ''
-    
-            for (let i = 0; i < arr.length; i++) {
-                palavra += arr[i]
-            }
-    
-            return palavra
-        }
-    
-        if (invertePalavras(str) === palavras(str)) {
-            verdadeiro = true
-        }
-    
-        return verdadeiro
+        return palavra
     }
+    
+    function palavras (str) {
+        str = str.toLowerCase()
+        str = str.replace(/\s/g,'')
+        let arr = str.split('')
+        let palavra = ''
+    
+        for (let i = 0; i < arr.length; i++) {
+                palavra += arr[i]
+        }
+    
+        return palavra
+    }
+    
+    if (invertePalavras(str) === palavras(str)) {
+            verdadeiro = true
+    }
+    
+    return verdadeiro
 }
 
-console.log(isPalindrome('A torre da derrota'))
+isPalindrome('A torre da derrota')
 
 function arrayMaxMin(arr){
     /* Seu código aqui */
